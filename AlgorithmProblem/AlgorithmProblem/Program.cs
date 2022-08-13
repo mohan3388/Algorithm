f4873829a60d41f80796567090cfd6008d80d68e
mohan <sahumohan338@gmail.com>
2022-08-13 11:36:26 -0700

    [mohan]add: Insertion sort

--- AlgorithmProblem/AlgorithmProblem/Program.cs
+++ AlgorithmProblem/AlgorithmProblem/Program.cs
@@ -5,26 +5,35 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to the program");
-            Console.WriteLine("1.Binary Serach Program\n2.Exit the program");
+            Console.WriteLine("1.Binary Serach Program\n2.Insertion sort program");
+            const string File_Path = @"F:\dotnet\algorith\Algorithm\AlgorithmProblem\AlgorithmProblem\List.txt";
             bool check = true;
             while (check)
             {
                 Console.WriteLine("Enter above option to execute");
+                Algorithm search = new Algorithm();
+                string words = File.ReadAllText(File_Path);
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
-                        const string File_Path = @"F:\dotnet\algorith\Algorithm\AlgorithmProblem\AlgorithmProblem\List.txt";
-                        string words = File.ReadAllText(File_Path);
+
+
                         string[] arr = words.Split(',');
                         Console.WriteLine("Enter word to Search");
                         string a = Console.ReadLine();
-                        Algorithm search = new Algorithm();
+
                         int result = search.BinarySearch(arr, a);
                         if (result == -1)
                             Console.WriteLine("Element not present");
                         else
                             Console.WriteLine("Element found at " + result);
+                        break;
+                    case 2:
+                        string[] arr2 = words.Split(',');
+                        InsertionSort problem = new InsertionSort();
+                        problem.Insertion(arr2);
+                        problem.Display(arr2);
                         break;
                     default:
                         check = false;
